e38480fef40eafd23ef23d0d420c08d5130a7265
Hoang NGUYEN <baauco@gmail.com>
2022-11-30 00:33:21 +0700

    feat(linux): add script to enable rpm fusion

--- linux/install_dnf.sh
+++ linux/install_dnf.sh
@@ -33,3 +33,13 @@ function install_pyenv {
   eval "$(pyenv init --path)"
   eval "$(pyenv init -)"
 }
+
+function install_rpm_fusion {
+  sudo dnf install -y \
+    "https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm" \
+    "https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm"
+  sudo dnf upgrade --refresh
+  sudo dnf -y groupupdate core
+  sudo dnf install -y rpmfusion-free-release-tainted \
+    rpmfusion-nonfree-release-tainted
+}
