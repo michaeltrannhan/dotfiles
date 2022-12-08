f683538048cb69693e6cc4cd59087514cca4edeb
Hoang NGUYEN <baauco@gmail.com>
2022-12-08 20:47:36 +0700

    refactor: split `python-venv` helper functions

--- windows/runcoms/profile.d/50-pyenv.rc.psm1
+++ windows/runcoms/profile.d/50-pyenv.rc.psm1
@@ -1,26 +1,22 @@
 Set-Alias -Name 'vx' -Value 'deactivate'
 
 function va {
-  param(
-    [Parameter(ValueFromRemainingArguments = $true)]
-    $venvPath = '.venv'
-  )
+  $venvPath = if ($args) { $args } else { '.venv' }
 
   if ((Get-Item -Path "$venvPath" -ErrorAction SilentlyContinue).PSIsContainer) {
     & ".\$venvPath\Scripts\Activate.ps1"
+    return
   }
-  else {
-    python -m venv --upgrade-deps "$venvPath"
-    & ".\$venvPath\Scripts\Activate.ps1"
-    # --upgrade-deps already includes pip and setuptools
-    & ".\$venvPath\Scripts\pip.exe" install wheel
 
-    $requirementsFile = 'requirements.txt'
-    if (Test-Path -Path "$requirementsFile" -PathType Leaf) {
-      & ".\$venvPath\Scripts\pip.exe" install -r "$requirementsFile"
-    }
-    else {
-      Add-Content -Path "$requirementsFile" -Value $null
-    }
+  python -m venv --upgrade-deps "$venvPath"
+  & ".\$venvPath\Scripts\Activate.ps1"
+  # --upgrade-deps already includes pip and setuptools
+  & ".\$venvPath\Scripts\pip.exe" install wheel
+
+  $requirementsFile = 'requirements.txt'
+  if (Test-Path -Path "$requirementsFile" -PathType Leaf) {
+    & ".\$venvPath\Scripts\pip.exe" install -r "$requirementsFile"
+  } else {
+    Add-Content -Path "$requirementsFile" -Value $null
   }
 }
